b73e9a75484d692106ff55c963e03a18e8a80381
Rajaf Dereje <149234613+raj-da@users.noreply.github.com>
2024-07-10 22:01:42 +0300

    Create main.py

---
+++ Hangman_game/main.py
@@ -0,0 +1,44 @@
+import random
+from hangman_art import *
+from hangman_words import word_list
+
+print(logo)
+print("Welcom player")
+
+word = random.choice(word_list)
+word_length = len(word)
+print("Choosen word:", word)
+
+life = 6
+display = []
+
+for i in range(word_length):
+    display += "_"
+
+game_over = False
+
+while not game_over:
+    guess = input("guess the letter: ")
+    if guess in display:
+        print("You've already gussed this letter!")
+
+    for i in range(word_length):
+        if word[i] == guess:
+            display[i] = guess
+    
+    print(' '.join(display)) # to concatinate the list into a single string
+    
+    if guess not in display:
+        print("You've guessed wrong!")
+        life -= 1
+        if life == 0:
+            print("You've lost")
+            game_over = True
+
+    if "_" not in display:
+        game_over = True
+        print("You've won")
+
+    print(stages[life])
+   
+    
